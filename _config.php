905188054e827970219a7c5ad37de656e2b2bf9a
csilverman <chris@csilverman.com>
2021-07-22 16:59:21 -0400

    Initial commit

---
+++ _config.php
@@ -0,0 +1,8 @@
+<?php
+
+// define("SITE__CUSTOM_HEADER_HOME", "<b class='main-title'>Summer<br />Immersion</b><b class='sub-title'> in the</b> <b class='sub-title'>Liberal Arts</b>");
+
+define('SITE__TYPEKIT_ID', 'afi7ukw');
+define('SITE__HIDE_TAGLINE', false);
+define('POST__SHOW_CATEGORIES', true);
+define('POST__CAT_DELIMITER', '');
---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Vassar functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Vassar
+ */
+
+/**
+ * Enqueue scripts and styles.
+ */
+function local_scripts() {
+	wp_enqueue_script( 'vassar-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'vassar-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_style( 'flickity-style' );
+	wp_enqueue_script( 'flickity' );
+	wp_enqueue_script( 'waypoints' );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'local_scripts' );
+
+// The action callback function.
+function mc_signup() {
+    include(get_stylesheet_directory().'/assets/includes/mailchimp.html');
+}
+add_action( 'vassarparent__after_entryContent', 'mc_signup', 10, 2 );
